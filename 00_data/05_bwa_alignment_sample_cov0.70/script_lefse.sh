f392d2163838ed96383ce9f5ecf03ba6ad2d3e7f
LuiguiGallardo <bfllg77@gmail.com>
2022-06-28 11:59:28 -0700

    LEfSe analysis added

--- 00_data/05_bwa_alignment_sample_cov0.70/script_lefse.sh
+++ 00_data/05_bwa_alignment_sample_cov0.70/script_lefse.sh
@@ -5,8 +5,9 @@
 # Date: 27.06.2022
 
 # Beginning of the script
-lefse-format_input.py allsamples_phages_cov0.70_relab_lefse.txt allsamples_phages_cov0.70_relab_lefse.in -u 1 -c 2
+lefse_format_input.py allsamples_phages_cov0.70_relab_lefse.txt allsamples_phages_cov0.70_relab_lefse.in -u 1 -c 2 -o 1000000
 
-``
+lefse_run.py allsamples_phages_cov0.70_relab_lefse.in allsamples_phages_cov0.70_relab_lefse.res -l 0
+
 # The end!
 
